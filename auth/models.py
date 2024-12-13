48e3e54c3d262a5828ffb583b827dd29a0e2c7cf
M24SOM <mmfarah@proton.me>
2024-12-13 16:51:02 +0300

    update tags review auth

---
+++ auth/models.py
@@ -0,0 +1,35 @@
+import uuid
+from datetime import date, datetime
+from typing import List, Optional
+
+import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Column, Field, Relationship, SQLModel
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+    uid: uuid.UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid.uuid4)
+    )
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    role: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
+    )
+    is_verified: bool = Field(default=False)
+    password_hash: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False), exclude=True
+    )
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    books: List["Book"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+    reviews: List["Review"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"lazy": "selectin"}
+    )
+
+    def __repr__(self):
+        return f"<User {self.username}>"
